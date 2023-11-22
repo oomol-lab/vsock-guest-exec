192ebc7c45e507ef2432572be7142fcb96be371d
Black-Hole1 <bh@bugs.cc>
2023-11-22 14:31:30 +0800

    feat(cmd): add command line params support
    EXAMPLES:
    
    ```shell
    $ ./vsock_guest_exec 1:2049 # cid is 2, vsock port is 2049
    $ ./vsock_guest_exec -1: # cid set to -1 (VMADDR_CID_ANY), vsock port is
    default(1024)
    $ ./vsock_guest_exec :1025 # vsock port set to 1025, cid is default(2)
    
    $ ./vsock_guest_exec foo:bar # cid is default(2), vsock port is default(1024)
    ```
    
    Signed-off-by: Black-Hole1 <bh@bugs.cc>

--- main.c
+++ main.c
@@ -10,10 +10,46 @@
 
 #define MAX_COMMAND_LENGTH 2048
 
-int main() {
-    int cid, sockfd;
+void setCID(char params[], int *cid) {
+    if (strlen(params) > 0) {
+        int _cid = atoi(params);
+        if (_cid == VMADDR_CID_ANY || _cid == VMADDR_CID_HYPERVISOR || 
+            _cid == VMADDR_CID_LOCAL || _cid == VMADDR_CID_HOST) {
+            *cid = _cid;
+        }
+    }
+}
+
+void setPort(char params[], int *port) {
+    if (strlen(params) > 0) {
+        int _port = atoi(params);
+        if (_port >= 0 && _port <= 65535) {
+            *port = _port;
+        }
+    }
+}
+
+int main(int argc, char *argv[]) {
+    int cid = VMADDR_CID_HOST;
+    int vsockPort = 1024;
+    int sockfd;
     struct sockaddr_vm sa_vm;
     char command[MAX_COMMAND_LENGTH];
+
+    if (argc > 1) {
+        char *param = argv[1];
+        char *delim = strchr(param, ':');
+        if (delim != NULL) {
+            *delim = '\0';
+            char *cidString = param;
+            char *portString = delim + 1;
+
+            setCID(cidString, &cid);
+            setPort(portString, &vsockPort);
+        } else {
+            setCID(param, &cid);
+        }
+    }
 
     sockfd = socket(AF_VSOCK, SOCK_STREAM, 0);
     if (sockfd < 0) {
@@ -23,8 +59,8 @@ int main() {
 
     memset(&sa_vm, 0, sizeof(struct sockaddr_vm));
     sa_vm.svm_family = AF_VSOCK;
-    sa_vm.svm_cid = VMADDR_CID_HOST;
-    sa_vm.svm_port = 1024;
+    sa_vm.svm_cid = cid;
+    sa_vm.svm_port = vsockPort;
 
     if (connect(sockfd, (struct sockaddr*)&sa_vm, sizeof(struct sockaddr_vm)) < 0) {
         perror("Failed to connect");
